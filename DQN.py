26f07fac1be4e14875222d864f65957d323ba232
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-04-11 18:00:56 -0400

    working DQN

--- DQN.py
+++ DQN.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import torch
 import random
 import torch.nn as nn
@@ -27,14 +29,13 @@ class ReplayMemory:
     def __init__(self, max_size, batch_size):
         self.max_size = max_size
         self.batch_size = batch_size
-        self.memory = []
+        self.memory = deque([], maxlen=max_size)
 
     def __len__(self):
         return len(self.memory)
 
     def add_to_memory(self, state, action, next_state, reward, done):
-        if len(self.memory) < self.max_size:
-            self.memory.append((state, action, next_state, reward, done))
+        self.memory.append((state, action, next_state, reward, done))
 
     def random_batch(self):
         return random.sample(self.memory, self.batch_size)
@@ -48,6 +49,8 @@ class Agent:
                  learning_rate,
                  num_episodes,
                  eps=1,
+                 min_eps=0.01,
+                 eps_decay=0.999,
                  gamma=0.99):
         self.env = env
         self.num_episodes = num_episodes
@@ -55,6 +58,8 @@ class Agent:
         self.batch_size = batch_size
         self.memory = ReplayMemory(memory_size, self.batch_size)
         self.eps = eps
+        self.min_eps = min_eps
+        self.eps_decay = eps_decay
         self.device = torch.device("cuda:" + str(0) if torch.cuda.is_available() else "cpu")
         self.model = DQN(env.observation_space.n, env.action_space.n).to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
@@ -67,23 +72,30 @@ class Agent:
         # Greedy choice (exploitation)
         else:
             next_action = int(self.model(state).argmax().detach().cpu())
+
         return next_action
 
-    def update(self, state, target):
-        prediction = self.model(state)[0]
+    def update(self, state, target, action):
+        prediction = self.model(state)[action]
         loss = F.smooth_l1_loss(prediction, target)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+    def get_target(self, reward, done, next_state):
+        # y = r if done
+        target = torch.tensor(reward).to(self.device)
+        if not done:
+            # y = r + gamma * max Q(s',a') if not done
+            next_state_max_Q = self.model(next_state).max()
+            target = (next_state_max_Q * self.gamma) + reward
+        return target
+
     def replay(self):
         batch = self.memory.random_batch()
         for state, action, next_state, reward, done in batch:
-            target = reward
-            if not done:
-                next_state_max_Q = self.model(next_state).max()
-                target = (next_state_max_Q * self.gamma) + reward
-            self.update(state, target)
+            target = self.get_target(reward, done, next_state)
+            self.update(state, target, action)
 
     def ohe(self, state):
         ohe_state = torch.zeros(self.env.observation_space.n).to(self.device)
@@ -91,30 +103,38 @@ class Agent:
         return ohe_state
 
     def train(self):
+        rewards_lst = []
         for i in range(self.num_episodes):
-            state = self.ohe(env.reset())
             rewards = 0
             done = False
+            state = self.ohe(self.env.reset())
             while not done:
                 action = self.eps_greedy_action(state)
                 next_state, reward, done, info = self.env.step(action)
+                rewards += reward
                 next_state = self.ohe(next_state)
-                next_state_max_Q = self.model(next_state).max()
+                self.memory.add_to_memory(state, action, next_state, reward, done)
+
                 if len(self.memory) > self.batch_size:
                     self.replay()
-                target = (next_state_max_Q * self.gamma) + reward
-                self.update(state, target)
-                self.eps = self.eps * 0.99
-                rewards += reward
-                self.memory.add_to_memory(state, action, next_state, reward, done)
+
+                target = self.get_target(reward, done, next_state)
+                self.update(state, target, action)
+                self.eps = min(self.min_eps, self.eps * self.eps_decay)
+
+                rewards_lst.append(rewards)
                 state = next_state
+
+            if not i % 10 and i != 0:
+                print('Episode {} Reward: {} Reward Rate {}'.format(i, rewards, str(sum(rewards_lst) / i)))
+
         self.env.close()
 
 
 if __name__ == "__main__":
     env = gym.make("FrozenLake-v0")
-    memory_size = 64
-    num_episodes = 5
+    memory_size = 1000
+    num_episodes = 10000
     batch_size = 32
     gamma = 0.99
     learning_rate = 0.001
