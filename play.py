aa6d9b7ac37bcdaf045a8c628bda7074ea0c8168
Francis Granger <franki_le_pro@hotmail.com>
2020-04-12 02:26:27 -0400

    add clean version

--- play.py
+++ play.py
@@ -4,16 +4,14 @@ import random
 
 env = gym.make("blokus:blokus-v0")  # Make sure to do: pip install -e blokus in root
 observation = env.reset()
-# for _ in range(100):
 done = False
 while not done:
     env.render()
-    actions = env.ai_possible_moves()  # your agent here (this takes random actions)
+    actions = env.ai_possible_moves()
     if len(actions) == 0:
         break
-    # action = env.action_space.sample()  # your agent here (this takes random actions)
+
     observation, reward, done, info = env.step(random.choice(actions))
-    # observation, reward, done, info = env.step(None)
 
     if done:
         observation = env.reset()
