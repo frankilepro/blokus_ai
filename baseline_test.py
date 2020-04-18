69ee58ed9595f7cb08de73b2806bda3c494dbad7
Francis Granger <franki_le_pro@hotmail.com>
2020-04-18 11:50:08 -0400

    test on colab

--- baseline_test.py
+++ baseline_test.py
@@ -1,25 +1,25 @@
 import gym
 
 from stable_baselines.common.env_checker import check_env
-# from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.deepq.policies import MlpPolicy, LnMlpPolicy
+from stable_baselines.common.policies import MlpPolicy
+# from stable_baselines.deepq.policies import MlpPolicy, LnMlpPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import A2C, DQN
 
-env = gym.make("blokus:blokus-v0")
+# env = gym.make("blokus:blokus-v0")
 # check_env(env)
 # exit(1)
 
 # Parallel environments
-# env = make_vec_env("blokus:blokus-v0", n_envs=4)
+env = make_vec_env("blokus:blokus-v0", n_envs=4)
 
 print("starting training")
-model = DQN(LnMlpPolicy, env, verbose=1)
-# model = A2C(MlpPolicy, env, verbose=1)
+# model = DQN(LnMlpPolicy, env, verbose=1)
+model = A2C(MlpPolicy, env, verbose=1, tensorboard_log="logdir")
 model.learn(total_timesteps=25000)
 model.save("blokus_weights")
 print("finish training")
-exit(1)
+input()
 
 del model  # remove to demonstrate saving and loading
 
