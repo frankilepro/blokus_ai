4178c322ce5fe0b5783c9306361e446b7ffcb1f7
Francis Granger <franki_le_pro@hotmail.com>
2020-04-18 13:32:11 -0400

    version 0.11

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 setup(
     name='blokus',
     packages=['blokus'],
-    version='0.1',
+    version='0.11',
     license='gpl-3.0',
     description='OpenAI gym environment for Blokus',
     url='https://github.com/frankilepro/blokus-ai',
-    download_url='https://github.com/frankilepro/blokus-ai/archive/v0.1.tar.gz',
+    download_url='https://github.com/frankilepro/blokus-ai/archive/v0.11.tar.gz',
     keywords=['blokus', 'board game', 'block us'],
     install_requires=[
         'gym',
